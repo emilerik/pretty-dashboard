bb7dd83b20f48302a3a3499038eac9254c64e1d6
emilerik <emil.eriksson41@gmail.com>
2019-03-18 17:29:52 +0100

    First commit

---
+++ src/containers/App.js
@@ -0,0 +1,15 @@
+import React, { Component } from "react";
+import Time from "../components/Time";
+import "./App.css";
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <Time />
+      </div>
+    );
+  }
+}
+
+export default App;
